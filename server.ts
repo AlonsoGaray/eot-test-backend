b5b94092a61f4b3ae2edb0f9ffa5ea90cecd04d2
Alonso Garay <alonsogarayp@gmail.com>
2023-12-19 02:54:56 -0500

    Update CORS configuration

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ import dotenv from 'dotenv'
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }))
+app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
