be190a35b4a2f730088085ae9e0b1b46ca9014f5
Alonso Garay <alonsogarayp@gmail.com>
2023-12-19 01:29:53 -0500

    delete config

--- config.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-export const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET!
-export const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET!
-export const isProduction = process.env.NODE_ENV! === 'production'
-export const baseDomain = process.env.BASE_DOMAIN!
